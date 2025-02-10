b09290a3c8b09aaca8eedc2d27ef5b1884500bb5
Pankaj Maurya <100466858+Pankaj352@users.noreply.github.com>
2025-02-10 10:30:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 function convertToRoman(num) {
     const romanNumerals = [
         ['M', 1000], 
-		['CM', 900], ['D', 500], ['CD', 400],
+        ['CM', 900], ['D', 500], ['CD', 400],
         ['C', 100], ['XC', 90], ['L', 50], ['XL', 40],
         ['X', 10], ['IX', 9], ['V', 5], ['IV', 4], ['I', 1]
     ];
     
-    if (num <=0 || num > 100000) {
+    if (num < 0 || num > 100000) {
         return "Enter a valid number from 0 to 100000";
     }
     
+    if (num === 0) {
+		return "N";
+	}
+
     let result = "";
     for (const [romanSymbol, value] of romanNumerals) {
         while (num >= value) {
