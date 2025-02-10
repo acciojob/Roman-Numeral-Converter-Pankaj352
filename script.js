bdaa04644b6488a795828f2d5a854e1d26d8ed1b
Pankaj Maurya <100466858+Pankaj352@users.noreply.github.com>
2025-02-10 10:24:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,27 @@
 function convertToRoman(num) {
-    const obj = {
-        0: ['M', 1000],
-        1: ['CM', 900],
-        2: ['D', 500],
-        3: ['CD', 400],
-        4: ['C', 100],
-        5: ['XC', 90],
-        6: ['L', 50],
-        7: ['XL', 40],
-        8: ['X', 10],
-        9: ['IX', 9],
-        10: ['V', 5],
-        11: ['IV', 4],
-        12: ['I', 1]
-    };
+    const romanNumerals = [
+        ['(C)', 100000], ['(L)', 50000], ['(X)', 10000], ['(V)', 5000],
+        ['M', 1000], ['CM', 900], ['D', 500], ['CD', 400],
+        ['C', 100], ['XC', 90], ['L', 50], ['XL', 40],
+        ['X', 10], ['IX', 9], ['V', 5], ['IV', 4], ['I', 1]
+    ];
     
-    if (num<0 || num>100000) {
-		return "enter valid number";
-	}
-	let result = "";
-    for (let key in obj) {
-        let [romanSymbol, value] = obj[key];
-        while (num>=value) {
-            result+=romanSymbol;
-            num-=value;
-        }
+    if (num <=0 || num > 100000) {
+        return "Enter a valid number from 0 to 100000";
     }
     
+
+    let result = "";
+    for (const [romanSymbol, value] of romanNumerals) {
+        while (num >= value) {
+            result += romanSymbol;
+            num -= value;
+        }
+    }
     return result;
 }
 
 function convertAndDisplay() {
-    let num = document.getElementById("numberInput").value;
-    document.getElementById("result").textContent = convertToRoman(parseInt(num));
+    let num = parseInt(document.getElementById("numberInput").value);
+    document.getElementById("result").textContent = convertToRoman(num);
 }
